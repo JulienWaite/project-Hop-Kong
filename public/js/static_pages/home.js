03f0201704a5900203b886edea715d93d8a31fbf
Julien Waite <julienwaite@gmail.com>
2016-02-21 23:13:48 +0800

    complete

--- public/js/static_pages/home.js
+++ public/js/static_pages/home.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     $('#find-vendor-btn').off().on("click", function (e) {
       e.preventDefault();
 
-      var locality    = $("#hk-locality").val();
       var vendorType  = $("#vendor-type").val();
+      var locality    = $("#hk-locality").val();
       var beerCountry = $("#beer-country").val();
 
       console.log(locality, vendorType, beerCountry);
@@ -24,20 +24,17 @@ $(document).ready(function () {
     });
   };
 
-
   // utility function
   var createNewImage = function (id, image, name) {
     var newLine = '' +
       '<div class="col-lg-3 col-md-4 col-xs-6 thumb">'+
-      '<a class="thumbnail" href="#">'+
+      '<a class="thumbnail" href="/vendors/'+ id +'">'+
       '<img class="img-responsive"' + ' src=' + image + ' alt="' + name +'">' +
       '</a>' +
       '</div>';
 
     $('#images').append(newLine);
   };
-
-
 
   var getAllVendors = function () {
     $.ajax({
@@ -46,20 +43,14 @@ $(document).ready(function () {
       success: function (response, status) {
         response.forEach(function (elem) {
           createNewImage(elem._id, elem.image, elem.name);
+          console.log(elem._id, elem.image, elem.name);
         });
-        //bindButtons();
       },
       error: function (response, status) {
         console.log(response);
       }
     });
   };
-
-
-
-
-
-
 
   var init = function () {
     getAllVendors();
--- routes/static_pages.js
+++ routes/static_pages.js
@@ -1,4 +1,5 @@
 var Authenticated = require("./modules/Authenticated.js");
+var Joi    = require('joi'); // Carmen code
 
 exports.register = function (server, options, next) {
   server.route([
@@ -40,7 +41,29 @@ exports.register = function (server, options, next) {
           reply.view('static_pages/bookmarks', data).code(200);
         });
       }
-    }
+    },
+    // CARMEN CODE
+    { // Get ONE vendor
+     method: 'GET',
+     path: '/vendors/{id}',
+     handler: function (request, reply) {
+       Authenticated(request, function (result) {
+         console.log(request);
+         var db = request.server.plugins['hapi-mongodb'].db;
+         var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
+
+         var id = ObjectID(request.params.id);
+
+         db.collection('vendors').findOne({"_id": id}, function (err, vendor) {
+           if (err) { return reply(err); }
+           // reply(results).code(200);
+           reply.view('static_pages/vendors', {vendor: vendor, authenticated: result.authenticated}).code(200);
+         });
+       });
+     }
+   }
+   //END OF CARMEN CODE
+
   ]);
 
   next();
