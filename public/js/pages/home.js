d017d63ebdcc9fd6af86a99d6886b7fcacaf271f
Julien Waite <julienwaite@gmail.com>
2016-02-22 14:23:39 +0800

    Partially done

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -6,13 +6,37 @@ $(document).ready(function () {
     });
   };
 
-$('#vendor-type').on('change', function(){console.log("it changed"); });
-  // $('#vendor-type').on('change', function(){
-  //  console.log("it changed");
-  // });
 
+  var $vendortype = $('#vendor-type');
+  //$('#vendor-type').on('change', function(){console.log("it changed"); });
+  $('#vendor-type').on('change', function(){console.log($vendortype.val());
 
-//console.log('#vendor-type').val();
+    if (($vendortype.val()) === "Brewery") {
+      console.log("Hello");
+      $('#beer-country').selectpicker('val', 'Hong Kong');
+      $('#hk-locality').selectpicker('val', 'All locations');
+      $('#beer-country').prop('disabled', true);
+      $('#beer-country').selectpicker('refresh');
+      $('#hk-locality').prop('disabled', true);
+      $('#hk-locality').selectpicker('refresh');
+    }
+    if (($vendortype.val()) === "Online Store") {
+      console.log("Hello");
+      $('#hk-locality').selectpicker('val', 'All locations');
+      $('#hk-locality').prop('disabled', true);
+      $('#hk-locality').selectpicker('refresh');
+    }
+    if (($vendortype.val()) === "Retail Store" || ($vendortype.val()) === "Craft Beer Bar") {
+      $('#hk-locality').prop('disabled', false);
+      $('#hk-locality').selectpicker('refresh');
+      $('#beer-country').prop('disabled', false);
+      $('#beer-country').selectpicker('refresh');
+    }
+    else console.log("It doesn't work");
+      // $('#vendor-type').on('change', function(){
+      //  console.log("it changed");
+      // });
+  });
 
   var init = function () {
     bindFindVendorButton();
