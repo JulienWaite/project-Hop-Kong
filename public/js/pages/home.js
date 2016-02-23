8213c0a59843bc71e50a83cc0f37ff93b9a817d4
Julien Waite <julienwaite@gmail.com>
2016-02-24 01:12:32 +0800

    clean up part 1

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -85,32 +85,7 @@ $(document).ready(function () {
     setFilterValFromStorage($beercountry, 'hopkongBeerCountry', 'All Countries');
   };
 
-
-
   init();
-  // var bindFindVendorButton = function () {
-  //   $('#find-vendor-btn').off().on("click", function (e) {
-  //     e.preventDefault();
-
-  //     var vendorType  = $("#vendor-type").val();
-  //     var locality    = $("#hk-locality").val();
-  //     var beerCountry = $("#beer-country").val();
-
-  //     console.log(locality, vendorType, beerCountry);
-
-  //     $.ajax({
-  //       method: "GET",
-  //       url: "/api/vendors",
-  //       success: function (response) {
-  //         console.log(response);
-
-  //         response.forEach(function (obj) {
-  //           console.log(obj.name);
-  //         });
-  //       }
-  //     });
-  //   });
-  // };
 
   // // utility function
   // var createNewImage = function (id, image, name) {
@@ -123,21 +98,4 @@ $(document).ready(function () {
 
   //   $('#images').append(newLine);
   // };
-
-  // var getAllVendors = function () {
-  //   $.ajax({
-  //     url: "/api/vendors",
-  //     method: "GET",
-  //     success: function (response, status) {
-  //       response.forEach(function (elem) {
-  //         createNewImage(elem._id, elem.image, elem.name);
-  //       });
-  //     },
-  //     error: function (response, status) {
-  //       console.log(response);
-  //     }
-  //   });
-  // };
-
-
 });
